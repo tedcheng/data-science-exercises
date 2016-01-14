5bc68766776af3e44826164b7857f0ab599797a3
tedcheng <yiqun.cheng@gmail.com>
2016-01-13 18:07:05 -0800

    add decision tree code

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,6 +24,15 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+print(len(features_train[0]))
+
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
+
+print(clf.score(features_test, labels_test))
+
+
 
 
 #########################################################
